68a8c2b5da75d1ca283453f40efc01745383d0c9
Juan11211 <avilesjuan628@gmail.com>
2023-12-07 10:25:09 -0500

    done

---
+++ webStorage/scripts/info.js
@@ -0,0 +1,21 @@
+"use strict"
+
+function setupPage() {
+   
+    const welcomeMsgEl = document.getElementById("welcomeMsg");
+    const role = sessionStorage.getItem("Role");
+    const textPEl = document.getElementById("textP");
+    const returnBtn = document.getElementById("return");
+  
+    welcomeMsgEl.textContent = `${role} ${sessionStorage.getItem("Name")}`;
+    if (role === "Student") textPEl.style.color = "blue";
+    else textPEl.style.color = "purple";
+    returnBtn.onclick = redirection;
+  }
+  
+  function redirection() {
+    location.href = "./index.html";
+  }
+
+  setupPage();
+  
